86562217ddc77e26c8a69f4cbb63fd7c1c740fc0
LINDSEY FUND <LINFUND@msn.com>
2023-10-13 18:42:01 -0400

    search design

--- SearchQuotes.Designer.cs
+++ SearchQuotes.Designer.cs
@@ -30,34 +30,75 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SearchQuotes));
             this.ReturnButton = new System.Windows.Forms.Button();
+            this.materialSearchCB = new System.Windows.Forms.ComboBox();
+            this.materialLabel = new System.Windows.Forms.Label();
+            this.searchButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // ReturnButton
             // 
-            this.ReturnButton.Font = new System.Drawing.Font("Lucida Sans", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ReturnButton.Location = new System.Drawing.Point(649, 399);
+            this.ReturnButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ReturnButton.Location = new System.Drawing.Point(487, 324);
+            this.ReturnButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.ReturnButton.Name = "ReturnButton";
-            this.ReturnButton.Size = new System.Drawing.Size(139, 39);
+            this.ReturnButton.Size = new System.Drawing.Size(104, 32);
             this.ReturnButton.TabIndex = 1;
             this.ReturnButton.Text = "Back To Home";
             this.ReturnButton.UseVisualStyleBackColor = true;
             this.ReturnButton.Click += new System.EventHandler(this.ReturnButton_Click);
             // 
+            // materialSearchCB
+            // 
+            this.materialSearchCB.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.materialSearchCB.FormattingEnabled = true;
+            this.materialSearchCB.Location = new System.Drawing.Point(264, 98);
+            this.materialSearchCB.Name = "materialSearchCB";
+            this.materialSearchCB.Size = new System.Drawing.Size(125, 21);
+            this.materialSearchCB.TabIndex = 2;
+            // 
+            // materialLabel
+            // 
+            this.materialLabel.AutoSize = true;
+            this.materialLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.materialLabel.Location = new System.Drawing.Point(162, 95);
+            this.materialLabel.Name = "materialLabel";
+            this.materialLabel.Size = new System.Drawing.Size(94, 24);
+            this.materialLabel.TabIndex = 3;
+            this.materialLabel.Text = "Materials: ";
+            // 
+            // searchButton
+            // 
+            this.searchButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchButton.Location = new System.Drawing.Point(231, 176);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(100, 40);
+            this.searchButton.TabIndex = 4;
+            this.searchButton.Text = "&Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            // 
             // SearchQuotes
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.materialLabel);
+            this.Controls.Add(this.materialSearchCB);
             this.Controls.Add(this.ReturnButton);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "SearchQuotes";
             this.Text = "Search for a Quote";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button ReturnButton;
+        private System.Windows.Forms.ComboBox materialSearchCB;
+        private System.Windows.Forms.Label materialLabel;
+        private System.Windows.Forms.Button searchButton;
     }
 }
