a9f5dd850a0ffd563b29076855feffb2eb643604
LINDSEY FUND <LINFUND@msn.com>
2023-10-14 12:07:46 -0400

    finished... i think.

--- SearchQuotes.cs
+++ SearchQuotes.cs
@@ -33,31 +33,47 @@ namespace MegaDesk_Montoya
         private void searchButton_Click(object sender, EventArgs e)
         {
             //get selected material
-            MaterialType mType = (MaterialType) materialSearchCB.SelectedIndex;
+            MaterialType searchByMatType = (MaterialType) materialSearchCB.SelectedIndex;
+            
+            //set up 'none found' alert
+            bool foundMatch = false;
 
             //loop through quotes
-            
             for (int i = 0; i < LoadedQuotes.Quotes.Count; i++)
             { 
                 //find by material
-                if (LoadedQuotes.Quotes[i].quoteDesk.material == mType)
+                if (LoadedQuotes.Quotes[i].quoteDesk.material == searchByMatType)
                 {
-                    //display quote
-                    //Console.WriteLine("Found it! :)");
-                    //Console.WriteLine(LoadedQuotes.Quotes[i]);
+                 //display quote
 
-                   
                     //string custName = LoadedQuotes.Quotes[i].customerName.ToString();
+                    //DateTime quoteDate = LoadedQuotes.Quotes[i].quoteDate......
+                    double deskWidth = LoadedQuotes.Quotes[i].quoteDesk.deskWidth;
+                    double deskDepth = LoadedQuotes.Quotes[i].quoteDesk.deskDepth;
+                    int deskDrawerCount = LoadedQuotes.Quotes[i].quoteDesk.deskDrawerCount;
                     string material = LoadedQuotes.Quotes[i].quoteDesk.material.ToString();
-                    decimal price = (decimal)LoadedQuotes.Quotes[i].quoteAmount;
+                    double price = LoadedQuotes.Quotes[i].quoteAmount;
+                    
+                    //set found to true so the none found won't display
+                    foundMatch = true;
 
-                    MessageBox.Show("Material: " + material + "\n" + "Total Price: $" + price);
+                    MessageBox.Show("Customer Name: " + "\n"
+                        + "Order Date of Quote:" + "\n"
+                        + "Width: " + deskWidth + "\n"
+                        + "Depth: " + deskDepth + "\n"
+                        + "Number of Drawers: " + deskDrawerCount + "\n"
+                        + "Material: " + material + "\n" 
+                        + "Total Price: $" + price);
 
                 }
                 
             }
 
-           //if no matches display a message box
+            //if no matches display a message box
+            if (foundMatch == false)
+            {
+                MessageBox.Show("No Matches Found");
+            }
 
         }
     }
